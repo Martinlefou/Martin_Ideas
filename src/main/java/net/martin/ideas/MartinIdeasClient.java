8c9c1f0227de5caa64dee15e23447dc6bca3f5e5
Martinlefou <guenardmartin966@gmail.com>
2024-10-29 10:59:49 +0100

    Initial commit

---
+++ src/main/java/net/martin/ideas/MartinIdeasClient.java
@@ -0,0 +1,10 @@
+package net.martin.ideas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MartinIdeasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
